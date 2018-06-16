bddb6d7523f39f8ccdf2a96fd4336d79e84be86d
Andrew Furth <andrew@versame.com>
2018-06-16 16:18:33 -0400

    root route goes to ShortenedUrl#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'stub_handler/show'
+  root 'shortened_urls#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :shortened_urls, path: 's'
