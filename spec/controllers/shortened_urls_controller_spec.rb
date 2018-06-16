fd81062a0c2867b10c589985ef23bec58fd0fa77
Andrew Furth <andrew@versame.com>
2018-06-16 18:12:13 -0400

    specs written to test invalid shortened url

--- spec/controllers/shortened_urls_controller_spec.rb
+++ spec/controllers/shortened_urls_controller_spec.rb
@@ -11,39 +11,27 @@ RSpec.describe ShortenedUrlsController, type: :controller do
         }.to change(ShortenedUrl,:count).by(1)
       end
 
-      it "renders the show page for the shortened url" do
+      it "renders the :show template" do
         post :create, params: { shortened_url: FactoryGirl.attributes_for(:shortened_url) }
         expect(response).to redirect_to ShortenedUrl.last
       end
 
     end
 
-    context 'invalid original url' do
+    context 'given an invalid original url' do
       it "does not create a new ShortenedUrl resource" do
+        expect{
+          post :create, params: { shortened_url: FactoryGirl.attributes_for(:shortened_url) }
+        }.to_not change(ShortenedUrl,:count)
       end
 
-      it "renders the new template" do
-      end
-
-      it "renders a flash message notifying user original url is invalid" do
+      it "renders the :new template" do
+        post :create, params: { shortened_url: FactoryGirl.attributes_for(:shortened_url) }
+        expect(response).to render_template :new
       end
     end
 
   end
 
-  # This would be a request to 'http://localhost:3000/a1b2c3'
-  describe "GET show" do
-    context "valid shortened url" do
-      it "redirects to the original url" do
-      end
-    end
 
-    context "invalid shortened url" do
-      it "redirects to the index route" do
-      end
-
-      it "renders a flash message notifying user stub does not exist" do
-      end
-    end
-  end
 end
