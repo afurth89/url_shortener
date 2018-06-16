91063d60449edcf92f4885a47e1081b70a8018db
Andrew Furth <andrew@versame.com>
2018-06-16 15:56:08 -0400

    generate stub after ShortenedUrl is created

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,3 +1,12 @@
 class ShortenedUrl < ApplicationRecord
 
+  after_create :generate_stub, unless: :stub?
+
+private
+  def generate_stub
+    update_column :stub, SecureRandom.urlsafe_base64(3)
+  rescue
+    retry
+  end
 end
+
