cc534b2c5b6fe9d432484e8e27279a903979b820
Andrew Furth <andrew@versame.com>
2018-06-16 15:47:22 -0400

    Created ShortenedUrl model; original_url saved from form input

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -3,6 +3,14 @@ class ShortenedUrlsController < ApplicationController
   end
 
   def create
-    render plain: params[:original_url].inspect
+    @shortened_url = ShortenedUrl.new(shortened_url_params)
+    @shortened_url.save
+    redirect_to new_shortened_url_path
+  end
+
+  private
+
+  def shortened_url_params
+    params.require(:shortened_url).permit(:original_url)
   end
 end
